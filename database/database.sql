deb9e332abb6308b071601fccdd61eee1d3ac0f9
KonstantinosPrt <konstantinos.pretsios24@gmail.com>
2023-11-23 19:21:06 +0200

    space deleted

--- database/database.sql
+++ database/database.sql
@@ -1,7 +1,7 @@
 -- SQLBook: Code
 DROP DATABASE IF EXISTS natural_disasters_volunteering_platform;
 CREATE DATABASE natural_disasters_volunteering_platform DEFAULT CHARSET utf16 COLLATE utf16_unicode_ci;
- USE natural_disasters_volunteering_platform;
+USE natural_disasters_volunteering_platform;
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`(
