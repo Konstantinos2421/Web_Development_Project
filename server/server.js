2dfa7720d147cf06c993a14fd139852186a426d2
KonstantinosPrt <konstantinos.pretsios24@gmail.com>
2023-12-19 11:37:48 +0200

    vc

---
+++ server/server.js
@@ -0,0 +1,74 @@
+import express from 'express';
+import mysql from 'mysql2';
+import cors from 'cors';
+import session from 'express-session';
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+let pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'natural_disasters_volunteering_platform'
+}).promise();
+
+app.use(session({
+    secret: 'secret-key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+app.post('/login/:username/:password', async (req, res) => {
+    const user = req.params.username;
+    const password = req.params.password;
+    
+    let [result] = await pool.query(`SELECT * FROM user WHERE username = ?`, [user]);
+    if (result.length == 0) {
+        res.send('Username not found');
+    }else{
+        if(result[0].password != password){
+            res.send('Wrong password');
+        }else{
+            [result] = await pool.query(`SELECT * FROM admin WHERE admin_username = ?`, [user]);
+            if(result.length == 0){
+                [result] = await pool.query(`SELECT * FROM rescuer WHERE rescuer_username = ?`, [user]);
+                if (result.length == 0) {
+                    res.send('citizen');
+                }else{
+                    res.send('rescuer');
+                }
+            }else{
+                res.send('admin');
+            }
+        }
+    }
+});
+
+app.get('/cargo/:user', async (req, res) => {
+    const user = req.params.user;
+    const [result] = await pool.query(`
+        SELECT * FROM cargo
+            JOIN product ON cargo.product_id = product.id
+            JOIN rescuer ON cargo.vehicle_name = rescuer.vehicle
+        WHERE rescuer.rescuer_username = ?
+    `, [user]);
+
+    res.json(result);
+});
+
+app.delete('/unloadall/:user', async (req, res) => {
+    const user = req.params.user;
+    const [result] = await pool.query(`
+        CALL rescuerUnloadCargoToBase(?)
+    `, [user]);
+
+    res.json(result);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
